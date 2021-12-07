a497c0d1bd2154b962b139acee9cc924dfe0fb82
DeepaDidharia <79315183+DeepaDidharia@users.noreply.github.com>
2021-12-07 17:06:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+export const firebaseConfig =  {
+
+  apiKey: "AIzaSyCB28gm6ZPBNU6a68W7fqokXm9QZ5DL3FU",
+  authDomain: "storytellingapp-63a33.firebaseapp.com",
+  projectId: "storytellingapp-63a33",
+  storageBucket: "storytellingapp-63a33.appspot.com",
+  messagingSenderId: "48838532792",
+  appId: "1:48838532792:web:6f0d13ab7c60aa9424de4d",
+  measurementId: "G-WD3QVVET04"
+  
+
+}
